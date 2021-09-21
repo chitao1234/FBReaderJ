366f723f0c22b39c4fc5e98b6f235ca1c63e487b
chitaotao <1139954766@qq.com>
2021-09-21 20:36:18 +0800

    added LICENSE, VERSION and random file from before

---
+++ obsolete/scripts/mergeAll.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+git checkout tags-master
+git merge master
+
+git checkout ice-cream-sandwich
+git merge master
+
+git checkout yota2
+git merge ice-cream-sandwich
+
+git checkout kindle
+git merge yota2
+
+git checkout nook
+git merge master
+
+git checkout microtypography
+git merge ice-cream-sandwich
+
+git checkout yotabeta
+git merge yota2
+
+git checkout betayota
+git merge yota2
+
+git checkout 2.6ics
+git merge ice-cream-sandwich
+
+git checkout 2.6
+git merge 2.6ics
+git merge yota2
+
+git checkout 2.6-kindle
+git merge 2.6
+git merge kindle
+
+git checkout master
